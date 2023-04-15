64ccc14139bcc0993e06abeb23a91608a0e808dc
anna hogue <annachogue@gmail.com>
2023-04-15 19:56:20 -0400

    fixed results.js to show the projects, edited links to open in new tab

--- react-portfolio/src/pages/projects/results.js
+++ react-portfolio/src/pages/projects/results.js
@@ -8,15 +8,17 @@ const Results = props => {
 
 	return (
 		Projects.map((project) => {
+			return(
 			<Card className="card">
 			<Card.Img className="card-img-top" src={ project.project_img } alt="project image"/>
 			<Card.Body>
 				<h5 className="card-title">{project.title}</h5>
-				<a href={project.app_link} className="btn btn-link">Application Link</a>
-				<a href={project.github_link} className="btn btn-link">GitHub Link</a>
+				<a href={project.app_link} className="btn btn-link" target="_blank">Application Link</a>
+				<a href={project.github_link} className="btn btn-link" target="_blank">GitHub Link</a>
 			</Card.Body>
 
 			</Card>
+			);
 		})
 	);
 };
